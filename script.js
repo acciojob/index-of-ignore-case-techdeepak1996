47db3d043b32100410e09958a9733867be729a6b
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2024-04-29 18:07:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // Convert both strings to lowercase for case-insensitive comparison
+  const lowerCaseS1 = s1.toLowerCase();
+  const lowerCaseS2 = s2.toLowerCase();
+
+  // Find the index of the substring in the lowercase string
+  const index = lowerCaseS1.indexOf(lowerCaseS2);
+
+  // Return the index or -1 if not found
+  return index;
 }
 
 // Please do not change the code below
